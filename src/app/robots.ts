65a104b8970f013c7633e0ad4352e3cfb3ef4f01
NguyenTrongPhuc <phucnguyentrong1804@gmail.com>
2025-07-28 13:00:12 +0700

    chore: update sitemap URL in robots configuration
    - Changed the sitemap URL in the robots.ts file to dynamically use the BASE_URL environment variable for improved flexibility and deployment consistency.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/"],
       },
     ],
-    sitemap: "https://keoduangocmai.com.vn/sitemap.xml",
+    sitemap: `${process.env.BASE_URL}/sitemap.xml`,
   };
 }
